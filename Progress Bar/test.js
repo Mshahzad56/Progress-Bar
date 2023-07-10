81d9ff72ecc74549029fc3c15e46ce5caf0f376b
Malik Shahzad <114597689+Mshahzad56@users.noreply.github.com>
2023-07-10 08:28:42 -0700

    Add files via upload

---
+++ Progress Bar/test.js
@@ -0,0 +1,22 @@
+// ? write a pogram to define js function with 3 parameteres?
+// ? write an arrow funtion  with string return type?
+// ? what is iffi?
+// ? use an arry method to sort this arry following array let a=[7,9,1.17,12]?
+// ? what is output type of (year==2017)?
+// ? what would be result if a= null ?? undefined?
+//? if(" "==""){}
+
+// console.log('myex'+"i love u")
+// console.log("me"+" ! i love u 2")
+
+
+{/* <   less Then 
+>   grater then 
+==  equal to 
+=== strickly equal
+!== not equlent
+<=  less then or equal to 
+>=  grater then or equal to 
+these always return (Boolean) */}
+
+// document.write("java script")
